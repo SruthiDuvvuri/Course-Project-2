630538f933eba32a47d36392b53b63e0afe5f1c1
SruthiDuvvuri <Sruthi.duvvuri@lntinfotech.com>
2014-07-22 19:02:13 +0530

    Update plot6.R

--- plot6.R
+++ plot6.R
@@ -4,20 +4,28 @@ NEI <- readRDS("summarySCC_PM25.rds")
 #Read the rds data for Source Classification Code Table
 SCC <- readRDS("Source_Classification_Code.rds")
 
-#Subsetting on baltimore city (fips=24510) and "on-road" Type
-NEIonRoad <- NEI[(NEI$fips=="24510") & (NEI$type=="ON-ROAD"), ]
+#Subsetting on baltimore city (fips=24510) and LA (fips=06037) with "on-road" Type
+NEIonRoad <- NEI[(NEI$fips %in% c("24510","06037")) & (NEI$type=="ON-ROAD"), ]
 
 #Filter out from all emissions on above criteria and calculate aggregate
-aggData <- aggregate(NEIonRoad$Emissions, list(Year=NEIonRoad$year), sum)
-aggData$x <- aggData$x/1000
+aggData <- aggregate(NEIonRoad$Emissions, list(Year=NEIonRoad$year,
+           Location=as.factor(NEIonRoad$fips)), sum)
+
+# To calculate which city has has seen greater changes over time in motor vehicle emissions
+# I am using a geometric-mean trick (taking differences of logs and then exponentiating)
+totData <- ddply(aggData,"Location",transform, Growth=c(0,(exp(diff(log(x)))-1)*100))
+#Substitute the fips code back to respective cities to make sense in graph
+totData<-as.data.frame(sapply(totData,gsub,pattern="06037",replacement="Los Angeles"))
+totData<-as.data.frame(sapply(totData,gsub,pattern="24510",replacement="Baltimore"))
+
 
 #Open graphics device
-png(filename="figure/plot5.png", width=480, height=480)
-#Plotting the details to answer how emissions changed from motor vecicle sources
-p <- ggplot(aggData) +
-        geom_line(aes(y = x, x = Year)) +
-        labs(y="Amount of PM2.5 emitted (tons)") +
-        ggtitle(expression(atop("Total PM2.5 emission from motor vehicle sources",
-                                atop(italic("Baltimore City, Maryland"), ""))))
-#shutdown device
+png(filename="figure/plot6.png", width=480, height=480)
+# Plotting The growth on 2 cities with ggplot
+ggplot(totData, aes(Year, Growth, fill = Location)) +
+        geom_bar(position = "dodge", stat="identity") +
+        labs(y="Variation (in %)") +
+        ggtitle(expression(atop(" variation of emission of PM2.5",
+                                atop(italic("from motor vehicle sources"), ""))))
+#shutting down the device
 dev.off()
